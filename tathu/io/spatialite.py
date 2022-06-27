ee46ece21fa012eb6fb8bc61af7860607ccd6920
Douglas Uba <douglas.uba@inpe.br>
2022-06-27 11:37:13 -0300

    For while, change date format

--- tathu/io/spatialite.py
+++ tathu/io/spatialite.py
@@ -346,7 +346,7 @@ class Loader(object):
 
             # Load numeric attributes
             s.name = uuid.UUID(row['name'])
-            s.timestamp = datetime.strptime(str(row['date_time']), '%Y-%m-%d %H:%M:%S')
+            s.timestamp = datetime.strptime(str(row['date_time']), '%Y-%m-%d %H:%M:%S.%f')
 
             for name in attrs:
                 s.attrs[name] = row[name]
