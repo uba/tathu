daee86a288829e7ed77130cc907b8e55d4228784
Douglas Uba <douglas.uba@inpe.br>
2022-06-27 11:39:44 -0300

    Add new example

---
+++ examples/system-history.py
@@ -0,0 +1,32 @@
+#
+# This file is part of TATHU - Tracking and Analysis of Thunderstorms.
+# Copyright (C) 2022 INPE.
+#
+# TATHU - Tracking and Analysis of Thunderstorms is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+#
+
+import sys
+
+# To use local package
+sys.path.append('../')
+
+from tathu.io import spatialite
+from tathu import visualizer
+
+# Setup informations to load systems from database
+dbname = 'systems-db.sqlite'
+table = 'systems'
+
+# Load family
+db = spatialite.Loader(dbname, table)
+
+# Get systems
+names = db.loadNames()
+
+# Load first
+family = db.load(names[0], ['min', 'mean', 'std', 'count'])
+
+# Visualize!
+view = visualizer.SystemHistoryView(family)
+view.show()
