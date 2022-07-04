39dd22c6110b01e4be709de08dd15b7a8134ee93
Douglas Uba <douglas.uba@inpe.br>
2022-07-04 17:32:49 -0300

    Add goes-downloader example

---
+++ examples/goes-downloader.py
@@ -0,0 +1,28 @@
+#
+# This file is part of TATHU - Tracking and Analysis of Thunderstorms.
+# Copyright (C) 2022 INPE.
+#
+# TATHU - Tracking and Analysis of Thunderstorms is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+#
+
+"""Example for TATHU - Tracking and Analysis of Thunderstorms."""
+
+import sys
+from datetime import datetime
+
+# To use local package
+sys.path.append('../')
+
+from tathu.downloader import goes
+from tathu.progress import TqdmProgress
+
+# List products
+products = goes.getProducts(goes.BUCKETS['GOES-16'])
+print('Products', *products, sep = '\n')
+
+# Download 08 April 2022, Channel 13, [00, 01, 02, 03] hours UTC
+start = end = datetime.strptime('20220408', '%Y%m%d')
+hours = ['00', '01', '02', '03']
+goes.download(goes.BUCKETS['GOES-16'], ['ABI-L2-CMIPF'], start, end,
+    hours, ['13'], './', progress=TqdmProgress('Download GOES data', 'files'))
