927b50e2a34cbf3540443021802fc35edfa2161d
Douglas Uba <douglas.uba@inpe.br>
2022-07-15 11:02:32 -0300

    Add GOES-13 download example

--- examples/goes-downloader.py
+++ examples/goes-downloader.py
@@ -20,11 +20,21 @@ hours = ['00', '01', '02', '03']
 # From AWS (full-disk)
 AWS.download(AWS.buckets['GOES-16'], ['ABI-L2-CMIPF'],
     start, end, hours, ['13'], './goes16-aws',
-    progress=TqdmProgress('Download GOES data (AWS)', 'files'))
+    progress=TqdmProgress('Download GOES-16 data (AWS)', 'files'))
 
 # From DISSM (crop/remapped version)
 hours = ['00', '01', '02', '03']
 DISSM.download('goes16', 'retangular/ch13',
     start, end, hours,
     './goes16-dissm/',
-    progress=TqdmProgress('Download GOES data (DISSM)', 'files'))
+    progress=TqdmProgress('Download GOES-16 data (DISSM)', 'files'))
+
+# 08 April 2015, Channel 04, [00, 01, 02, 03] hours UTC
+start = end = datetime.strptime('20150408', '%Y%m%d')
+
+# From DISSM (crop/remapped version - GOES-13)
+hours = ['00', '01', '02', '03']
+DISSM.download('goes13', 'retangular_4km/ch4_bin',
+    start, end, hours,
+    './goes13-dissm/',
+    progress=TqdmProgress('Download GOES-13 data (DISSM)', 'files'))
