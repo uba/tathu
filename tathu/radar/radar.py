7cfa08c0b14e3ff5bb222e5841a2534b18980cea
Douglas Uba <douglas.uba@inpe.br>
2022-08-10 16:57:42 -0300

    Starting with radars geospatial info

--- tathu/radar/radar.py
+++ tathu/radar/radar.py
@@ -6,6 +6,7 @@
 # under the terms of the MIT License; see LICENSE file for more details.
 #
 
+import os
 import numpy as np
 from osgeo import gdal
 
@@ -25,3 +26,85 @@ def read(path, extent, nlines, ncols):
     raster.FlushCache()
 
     return raster
+
+# Dic of radars geospatial info
+radars = {
+    'R13537439': {
+        'name': 'SaoRoque',
+        'llx': -49.5786,
+        'ury': -21.3379,
+        'dx':  0.00995700,
+        'dy': -0.00900140,
+        'ncols': 500,
+        'nlines': 500
+    },
+    'R13577441': {
+        'name': 'Cangucu',
+        'llx': -55.3873,
+        'ury': -29.1325,
+        'dx':  0.0107632,
+        'dy': -0.00901620,
+        'ncols': 500,
+        'nlines': 500
+    },
+    'R13507440': {
+        'name': 'Gama',
+        'llx': -50.3701,
+        'ury': -13.7188,
+        'dx':  0.00943480,
+        'dy': -0.00898860,
+        'ncols': 500,
+        'nlines': 500
+    },
+    'R12132246': {
+        'name': 'PicoDoCouto',
+        'llx': -45.7583,
+        'ury': -20.2013,
+        'dx':  0.00986420,
+        'dy': -0.00899940,
+        'ncols': 500,
+        'nlines': 500
+    },
+    'R13557444': {
+        'name': 'Santiago',
+        'llx': -57.5513,
+        'ury': -26.9560,
+        'dx':  0.0105054,
+        'dy': -0.00901180,
+        'ncols': 500,
+        'nlines': 500
+    },
+    'R12132246': {
+        'name': 'MorroDaIgreja',
+        'llx': -52.0632,
+        'ury': -25.8599,
+        'dx':  0.0103856,
+        'dy': -0.00900980,
+        'ncols': 500,
+        'nlines': 500
+    },
+    'R12132246': {
+        'name': 'Chapeco',
+        'llx': -55.1672,
+        'ury': -24.7816,
+        'dx':  0.0102743,
+        'dy': -0.00900776,
+        'ncols': 500,
+        'nlines': 500
+    }
+}
+
+# Compute extent for each radar
+for key in radars:
+    sensor = radars[key]
+    sensor['extent'] = [
+        sensor['llx'],
+        sensor['ury'] + sensor['dy'] * (sensor['nlines']),
+        sensor['llx'] + sensor['dx'] * (sensor['ncols']),
+        sensor['ury']
+    ]
+
+def getGeoSpatialInfo(path):
+    filename = os.path.basename(path)
+    preffix = filename.split('_')[0]
+    return radars[preffix]
