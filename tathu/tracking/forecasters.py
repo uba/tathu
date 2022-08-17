e036dfab18b3a6722aebeca7d0e46c7d98c8ff5a
Douglas Uba <douglas.uba@inpe.br>
2022-08-17 12:08:17 -0300

    Add option: forecast scaled

--- tathu/tracking/forecasters.py
+++ tathu/tracking/forecasters.py
@@ -22,7 +22,7 @@ def compute_distance(p1, p2):
 def compute_elapsed_time(sys):
     if not sys.relationships:
         return 0.0
-    return abs((sys.timestamp - sys.relationships[0].timestamp).seconds / 60)
+    return abs((sys.timestamp - sys.relationships[0].timestamp).seconds/60)
 
 def delta(p1, p2, elapsed_time=1.0):
     return (p1.GetX() - p2.GetX())/elapsed_time, (p1.GetY() - p2.GetY())/elapsed_time
@@ -45,13 +45,26 @@ def compute_last_centroid(sys):
 
     return centroid
 
+def compute_scale_factor(sys, elapsed_time=1.0):
+    if not sys.relationships:
+        return 0.0
+
+    current_area = sys.geom.GetArea()
+
+    previous_area = 0.0
+    for r in sys.relationships:
+        previous_area += r.geom.GetArea() 
+
+    return ((current_area - previous_area)/previous_area)/elapsed_time
+
 class Conservative(object):
     '''
     This class implements a conservative forecaster.
     '''
-    def __init__(self, previous, intervals):
+    def __init__(self, previous, intervals, applyScale=False):
         self.previous = previous # Set of previous systems at time.
         self.intervals = intervals # List of intervals for forecasting. Ex.: [15, 30, 45, 60] # minutes
+        self.applyScale = applyScale # A flag that indicates if forecast will be scaled.
 
     def forecast(self, current):
         # Create results
@@ -74,10 +87,15 @@ class Conservative(object):
             # Compute factor
             dx, dy = delta(sys.geom.Centroid(), base, elapsedtime)
 
+            ### Compute scale factor ###
+            scale = compute_scale_factor(sys, elapsedtime)
+
             # Apply transform (for each interval)
             for t in self.intervals:
                 prevision = copy.deepcopy(sys)
                 prevision.geom = transform.translate(sys.geom, dx * t, dy * t)
+                if self.applyScale:
+                    prevision.geom = transform.scale(prevision.geom, 1 + scale * t, 1 + scale * t)
                 forecasts[t].append(prevision)
 
         return forecasts
