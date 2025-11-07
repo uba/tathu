b845d45ab87e41fa6e0784a83551645e3b563a07
Douglas Uba <douglas.uba@inpe.br>
2025-11-07 13:19:33 -0300

    Add new constant

--- tathu/constants.py
+++ tathu/constants.py
@@ -11,6 +11,9 @@ from osgeo import osr
 # Define KM_PER_DEGREE
 KM_PER_DEGREE = 40075.16/360.0
 
+# Define METERS_PER_DEGREE
+METERS_PER_DEGREE = KM_PER_DEGREE * 1000.0
+
 # Define Lat/Lon WSG84 Spatial Reference System (EPSG:4326)
 LAT_LON_WGS84 = osr.SpatialReference()
 LAT_LON_WGS84.ImportFromProj4('+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs')
